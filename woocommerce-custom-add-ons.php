861eec098fe2be14b493ba466fa4347aa035ec62
Sumit Jha <developersumit99@gmail.com>
2024-11-20 23:36:34 +0545

    Initial setup for Woocommerce Custom addon plugin

---
+++ woocommerce-custom-add-ons.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Plugin Name:       WooCommerce Custom Add-ons Plugin
+ * Plugin URI:        https://sumitjha.info.np/
+ * Description:       Woocommerce Custom Add ons Plugin to add additional feature to ecommerce website.
+ * Version:           1.0
+ * Author:            Sumit Jha
+ * Author URI:        https://sumitjha.info.np/
+ */
